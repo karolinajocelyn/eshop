dff42a795145d2caada8534099f36cde854030af
karolinajocelyn <karolina.jocelyn@gmail.com>
2025-03-07 17:34:47 +0700

    [GREEN] Implemented payment repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -1,24 +1,49 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 public class PaymentRepository {
-    private Map<String, Payment> payments;
+    private List<Payment> paymentData = new ArrayList<>();
 
-    public PaymentRepository() {
-        this.payments = new HashMap<>();
+    public Payment save(Payment payment) {
+        int i = 0;
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(payment.getId())) {
+                paymentData.remove(i);
+                paymentData.add(i, payment);
+                return payment;
+            }
+            i++;
+        }
+        paymentData.add(payment);
+        return payment;
     }
 
-    public Payment findById(String id) {return null;}
+    public Payment findById(String id) {
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        return null;
+    }
 
-    public Payment save(Payment payment) {return null;}
+    public List<Payment> findAll() {
+        return new ArrayList<>(paymentData);
+    }
 
-    public List<Payment> findAll() {return null;}
-
-    public List<Payment> findByOrderId(String orderId) {return null;}
+    public List<Payment> findByOrderId(String orderId) {
+        List<Payment> result = new ArrayList<>();
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getOrder().getId().equals(orderId)) {
+                result.add(savedPayment);
+            }
+        }
+        return result;
+    }
 }
